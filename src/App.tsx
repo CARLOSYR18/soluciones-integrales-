ffde1eadad5ff7b32623616d954d73adfbe25d48
carlos <carlosdanielyamacacho@gamail.com>
2026-02-18 05:47:01 -0500

    Remove duplicate Router from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import './App.css';
 import './index.css';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SolucionesIntegralesJBSection from './components/SolucionesIntegralesJBSection';
-import Prestamos from './pages/Prestamos';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<SolucionesIntegralesJBSection className="bg-gray-50" />} />
-        <Route path="/prestamos" element={<Prestamos />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <SolucionesIntegralesJBSection className="bg-gray-50" />;
 }
 
 export default App;
