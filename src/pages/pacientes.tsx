4b4a4f97244b753bf6fc334c5065158c55fd96a5
carlos <carlosdanielyamacacho@gamail.com>
2026-02-18 05:28:31 -0500

    Add Pacientes page

---
+++ src/pages/pacientes.tsx
@@ -0,0 +1,8 @@
+export default function Pacientes() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Gestión de Pacientes</h1>
+      <button>Agregar Paciente</button>
+    </div>
+  );
+}
